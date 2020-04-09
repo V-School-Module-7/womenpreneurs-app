0d61c339114c7cdc7c2143f1dc52f9c765f64128
blockerpatrice <patricemblocker@yahoo.com>
2020-04-08 18:24:53 -0600

    Notes in README.md

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import PaymentForm from './PaymentForm.js'
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
+const stripePromise = loadStripe('ppk_test_wsLcr2VwAVtzK2j7CaSLMjR0');
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      womanpreneurs
+        Womanpreneurs
+
+      <Elements stripe={stripePromise}>
+        <PaymentForm/>
+      </Elements>
     </div>
   );
 }
-
 export default App;
